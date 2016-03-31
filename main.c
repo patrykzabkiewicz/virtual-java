62f0128c4b33d1e8ef6a40cd8650d64165bff853
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-03-31 09:01:47 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,35 @@
+/**************************
+ *
+ *	Lightweigh Java VM
+ *
+ *	Project is intended to provide a lightweight java virtual machine
+ *	Main features are:
+ *		- register based
+ *		- application isolation
+ *		- batch memory allocation from kernel
+ *		- dynamic memory allocation inside machine
+ *		- returning memory to kernel after garbage collect
+ *		- dynamic garbage collector
+ *		- 100% compatible with current java spec
+ *		- continue running after program closed
+ *		- prefetch resources
+ *		- predict resource allocation (not lazy allocation like normal java)
+ *		- resolve dependencies and load them once, use multiple times
+ *		- 
+ *
+ *	Author: 	Patryk Zabkiewicz
+ *	Date: 		2016-03-23
+ *
+ *	License:	GPL 3.0
+ *
+*/ 
+
+#include <stdio>
+
+int main(int argc, char *argv[]) {
+
+
+
+	/* properly always return zero at the end */
+	return 0;
+}
