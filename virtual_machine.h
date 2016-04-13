2fe1db8fc730a948e159ed6040cc1b9b3b5bca39
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-04-13 14:54:15 +0200

    structural changes

--- virtual_machine.h
+++ virtual_machine.h
@@ -35,36 +35,6 @@ typedef struct _stack {
 	uint8 * first;
 } PX_STACK;
 
-/* Machine structure */
-typedef struct _machine {
-	PX_CPU 		* cpu;		/* CPU of virtual machine */
-	PX_STACK 	* stack;	/* Stack of the virtual machine */
-	MEMORY		* mem;		/* Memory for the machine */
-	//NETWORK * net;			/* Network connection for virtual machine */
-	//DISK * io;				/* Disk storage for virtual machine */
-} MACHINE;
-
-typedef struct _machine_elem {
-  MACHINE * data;
-  MACHINE * next;
-  MACHINE * prev;
-} MACHINE_ELEM;
-
-
-/* Double ended list of machines */
-typedef struct _machine_list {
-  MACHINE_ELEM * elem;
-  MACHINE_ELEM * first;
-  MACHINE_ELEM * last;
-  uint32 count;
-
-  (void *)(*append_front)();
-  (void *)(*append_back)();
-  (void *)(*pop_back)();
-  (void *)(*pop_front)();
-
-} MACHINE_LIST;
-
 /* Structure that holds all the virtual machines abstraction */
 typedef struct _machines_struct {
   	MACHINE * machines_list;       /* List of the machines */
