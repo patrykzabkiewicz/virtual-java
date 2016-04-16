aa240cc4825a32c586bff9411ed10907407a7ea7
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-04-16 22:18:49 +0200

    sort added and few changes

---
+++ sort.h
@@ -0,0 +1,16 @@
+/*
+ * sort.h
+ *
+ *  Created on: 16 kwi 2016
+ *      Author: zabkiewi
+ */
+
+#ifndef SORT_H_
+#define SORT_H_
+
+/* quicksort */
+void sort(void * structure, void * comparator);
+
+
+
+#endif /* SORT_H_ */
