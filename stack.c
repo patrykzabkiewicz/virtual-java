8626bc4b9c6b585c6a9236f092f9285848583485
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-04-16 22:18:22 +0200

    stack changes

---
+++ stack.c
@@ -0,0 +1,56 @@
+/*
+ * stack.c
+ *
+ *  Created on: 16 kwi 2016
+ *      Author: zabkiewi
+ */
+
+#include "stack.h"
+
+/* initiates the stack structure */
+void stack_init(STACK * stack, uint32 elem_size) {
+	stack->copy = 0;
+	stack->elem_size = elem_size;
+	stack->first = 0;
+	stack->count = 0;
+}
+
+/* copies the stack and returns the pointer to new structure */
+STACK * stack_copy(STACK * stack) {
+	STACK * tmp;
+	tmp = stack;
+	tmp->copy = 1;
+	return tmp;
+}
+
+/* makes a hard copy of the stack */
+STACK * stack_hard_copy(STACK * stack) {
+	STACK * tmp;
+	tmp->copy = 0;
+	tmp->count = stack->count;
+	tmp->elem_size = stack->elem_size;
+	tmp->first = stack->first;
+	// copy all elements one by one
+	while(stack->first) {
+
+	}
+	return tmp;
+}
+
+/* push element to stack */
+void push_stack(STACK * stack, void * data) {
+	STACK_ELEM * elem;
+	elem->data = data;
+	stack->first->next = elem;
+}
+
+/* pop element from stack */
+void * pop_stack(STACK * stack) {
+	STACK_ELEM * tmp;
+	tmp = stack->first;
+	free(stack->first);
+	stack->first = tmp->next;
+	return tmp->data;
+}
+
+
--- stack.h
+++ stack.h
@@ -8,8 +8,33 @@
 #ifndef STACK_H_
 #define STACK_H_
 
-typedef struct _stack {
+#include "typedef.h"
 
+typedef struct _stack_elem {
+	void * data;
+	struct _stack_elem * next;
+} STACK_ELEM;
+
+typedef struct _stack {
+	STACK_ELEM * first;
+	uint32 elem_size;
+	uint32 count;
+	uint8 copy : 1;		/* copy-on-write idiom */
 } STACK;
+
+/* initiates the stack structure */
+void stack_init(STACK * stack);
+
+/* copies the stack and returns the pointer to new structure */
+STACK * stack_copy(STACK * stack);
+
+/* makes a hard copy of the stack */
+STACK * stack_hard_copy(STACK * stack);
+
+/* push element to stack */
+void push_stack(STACK * stack);
+
+/* pop element from stack */
+void * pop_stack(STACK * stack);
 
 #endif /* STACK_H_ */
