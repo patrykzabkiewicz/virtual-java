02f3d5921557fd9b1a3d2899ca88d4ac0c57d343
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-04-19 12:10:00 +0200

    chain added

---
+++ chain.c
@@ -0,0 +1,33 @@
+
+#include "chain.h"
+
+/* chain initializer */
+void chain_init(CHAIN * const chain, uint32 elem_size) {
+	chain->count = 0;
+	chain->elem_size = elem_size;
+	chain->first = 0;
+	chain->last = 0;
+	chain->max = 0;
+}
+
+/* chain append element in the back */
+void chain_append(CHAIN * const chain, CHAIN_ELEM * elem) {
+	chain->last->next = elem;
+	elem->prev = chain->last;
+	chain->last = elem;
+	chain->count++;
+}
+
+/* chain pop element from the back */
+size_t chain_pop(CHAIN * const chain, void * data) {
+	if(chain->last) {
+		data = chain->last;
+		chain->last = chain->last->prev;
+		chain->last->next = 0;
+		chain->count--;
+		return chain->elem_size;
+	} else {
+		return 0;
+	}
+}
+
---
+++ chain.h
@@ -0,0 +1,34 @@
+#ifndef CHAIN_H_
+#define CHAIN_H_
+
+#include "typedef.h"
+
+/* chain element structure */
+typedef struct _chain_elem {
+	uint32 id;					/* identification number */
+	uint32 timestamp;			/* time stamp */
+	void * data;				/* pointer to data object */
+	uint8 * hash;				/* hash value for data integration */
+	struct _chain_elem * next;	/* pointer to next element */
+	struct _chain_elem * prev;	/* pointer to previous element */
+} CHAIN_ELEM;
+
+/* chain structure */
+typedef struct _chain {
+	size_t elem_size;			/* single element size */
+	CHAIN_ELEM * first;			/* first element pointer */
+	CHAIN_ELEM * last;			/* last element pointer */
+	uint32 count;				/* quantity of elements */
+	uint32 max;					/* max amount of elements */
+} CHAIN;
+
+/* chain initializer */
+void chain_init(CHAIN * const chain, uint32 elem_size);
+
+/* chain append element in the back */
+void chain_append(CHAIN * const chain, CHAIN_ELEM * elem);
+
+/* chain pop element from the back */
+size_t chain_pop(CHAIN * const chain, void * data);
+
+#endif // CHAIN_H_
