893ce8eea4446dae8bf696333b011830c0a40026
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-05-06 10:32:38 +0200

    java class file parser

--- parser.h
+++ parser.h
@@ -6,34 +6,51 @@
 
 #define java_class_magic 0xCAFEBABE
 
-struct Class_File_Format {
-   u4 magic_number;
+typedef struct _cp_info {
+    uint32 cos;
+} cp_info;
 
-   u2 minor_version;   
-   u2 major_version;
+typedef struct _field_info {
+    uint32 cos;
+} field_info;
 
-   u2 constant_pool_count;   
+typedef struct _method_info {
+    uint32 cos;
+} method_info;
+
+typedef struct _attribute_info {
+    uint32 cos;
+} attribute_info;
+
+
+typedef struct Class_File_Format {
+   uint32 magic_number;
+
+   uint16 minor_version;   
+   uint16 major_version;
+
+   uint16 constant_pool_count;   // n+2 constans
   
    cp_info constant_pool[constant_pool_count - 1];
 
-   u2 access_flags;
+   uint16 access_flags;
 
-   u2 this_class;
-   u2 super_class;
+   uint16 this_class;
+   uint16 super_class;
 
-   u2 interfaces_count;   
+   uint16 interfaces_count;   
    
-   u2 interfaces[interfaces_count];
+   uint16 interfaces[interfaces_count];
 
-   u2 fields_count;   
+   uint16 fields_count;   
    field_info fields[fields_count];
 
-   u2 methods_count;
+   uint16 methods_count;
    method_info methods[methods_count];
 
-   u2 attributes_count;   
+   uint16 attributes_count;   
    attribute_info attributes[attributes_count];
-}
+} CLASS;
 
 #endif // PARSER_H_
 
