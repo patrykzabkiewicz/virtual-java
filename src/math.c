dc719327b9273b5d265b1e13acb59235f36836d8
Patryk Zabkiewicz <patryk.zabkiewicz@hpe.com>
2017-01-12 15:46:34 +0100

    math added

--- src/math.c
+++ src/math.c
@@ -8,7 +8,11 @@
 #include "math.h"
 
 void Vec10Copy( vec_t *in, vec_t *out ) {
-
+    int i;
+    int in_size = sizeof(in)/sizeof(in[0]);
+    for(i=0; i<in_size; i++) {
+        out[i]=in[i];
+    }
 }
 
 vec_t _DotProduct (vec3_t v1, vec3_t v2) {
@@ -17,19 +21,31 @@ vec_t _DotProduct (vec3_t v1, vec3_t v2) {
 }
 
 void _VectorSubtract (vec3_t va, vec3_t vb, vec3_t out) {
-
+    int i;
+    for(i=0; i<3; i++) {
+        out[i] = va[i] - vb[i];
+    }
 }
 
 void _VectorAdd (vec3_t va, vec3_t vb, vec3_t out) {
-
+    int i;
+    for(i=0; i<3; i++) {
+        out[i] = va[i] + vb[i];
+    }
 }
 
 void _VectorCopy (vec3_t in, vec3_t out) {
-
+    int i;
+    for(i=0; i<3; i++) {
+        out[i] = in[i];
+    }
 }
 
 void _VectorScale (vec3_t v, vec_t scale, vec3_t out) {
-
+    int i;
+    for(i=0; i<3; i++) {
+        out[i] = v[i] * scale;
+    }
 }
 
 double VectorLength( const vec3_t v ) {
